36ca2f7482ff4b18fca55b288d4c99e787f0ff44
hanchao5272 <89298141@qq.com>
2018-06-03 23:23:19 +0800

    优化Eureka相关代码与注释

--- eureka-hi/src/main/java/pers/hanchao/springclouddemo/eurekahi/EurekaHiApplication.java
+++ eureka-hi/src/main/java/pers/hanchao/springclouddemo/eurekahi/EurekaHiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +27,8 @@ public class EurekaHiApplication {
 	 * @param name
 	 * @return
 	 */
-	@GetMapping("/hi")
-	public String home(@RequestParam String name){
+	@GetMapping("/hi/{name}")
+	public String home(@PathVariable String name){
 		return "hi " + name + ",I am from port :" + port;
 	}
 }
